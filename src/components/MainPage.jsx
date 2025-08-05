bc2e099bcab26282393ad29891faee786723dca0
akshay-ken <akshaybest3120@gmail.com>
2025-08-05 18:06:03 +0530

    feat: statics card ready for maping data

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,9 +1,11 @@
+import { StaticsCard } from "./StaticsCard";
 import { UserCard } from "./UserCard";
 
 export function MainPage() {
   return (
-    <main className="w-full h-lvh flex flex-col items-center mt-24">
+    <main className="w-full h-lvh flex flex-col items-center gap-y-8 mt-24">
       <UserCard />
+      <StaticsCard />
     </main>
   );
 }
--- src/components/StaticsCard.jsx
+++ src/components/StaticsCard.jsx
@@ -1,3 +1,22 @@
+import dots from "../assets/images/icon-ellipsis.svg";
+import workBag from "../assets/images/icon-work.svg";
 export function StaticsCard() {
-  return <section></section>;
+  return (
+    <section className="w-sm flex flex-col bg-Navy-900 rounded-2xl">
+      <div className="bg-Orange-work basis-1/3 rounded-t-2xl">
+        <img src={workBag} className=" float-right" alt="" />
+      </div>
+      <div className="basis-2/3 bg-Navy-900 -mt-6 rounded-2xl p-6">
+        <div className="flex flex-row items-center justify-between ">
+          <p className="text-xl text-white">Work</p>
+
+          <img src={dots} className="w-6 h-2" alt="" />
+        </div>
+        <div className="flex flex-row mt-2 items-center justify-between">
+          <p className="font-thin text-3xl text-white">32hrs</p>
+          <p className="text-lg text-Navy-200">last week</p>
+        </div>
+      </div>
+    </section>
+  );
 }
